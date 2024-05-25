023693ddd6f2275de99d7e79419921d342aa77f0
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-25 12:05:41 -0300

    Criação da Model de Endereco

---
+++ Cp3-Cadastro/Models/EnderecoModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+
+namespace Cp3_Cadastro.Models
+{
+    [Table("Endereco")]
+    public class EnderecoModel
+    {
+        [Key]
+        public int id_endereco {  get; set; }
+
+        [Required]
+        public string? rua_endereco { get; set; }
+
+        public string? complemento_endereco { get; set; }
+
+        [Required]
+        public string? bairro_endereco { get; set; }
+
+        [Required]
+        public string? cidade_endereco { get; set; }
+
+        [Required]
+        public string? estado_endereco { get; set; }
+
+        [Required]
+        public string? cep_endereco { get; set; }
+
+        [ForeignKey("Pessoa")]
+        public int id_pessoa { get; set; }
+        public PessoaModel pessoa { get; set; }
+    }
+}
