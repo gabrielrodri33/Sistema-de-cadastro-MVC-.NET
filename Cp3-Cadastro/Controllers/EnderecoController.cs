1f115f79b403ed2b41e16e8855a15f67cb932599
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-25 13:21:57 -0300

    Criado controller de endereco

--- Cp3-Cadastro/Controllers/EnderecoController.cs
+++ Cp3-Cadastro/Controllers/EnderecoController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cp3_Cadastro.Models;
+using Cp3_Cadastro.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cp3_Cadastro.Controllers
 {
     public class EnderecoController : Controller
     {
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public EnderecoController(IEnderecoRepository enderecoRepository)
+        {
+            _enderecoRepository = enderecoRepository;
+        }
+
         public IActionResult Index()
         {
+            List<EnderecoModel> enderecos = _enderecoRepository.BuscarTodos();
             return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        
+        public IActionResult Editar(int id_endereco)
+        {
+            EnderecoModel endereco = _enderecoRepository.ListarPorId(id_endereco);
+            return View(endereco);
+        }
+
+        public IActionResult ApagarConfirmacao(int id_endereco)
+        {
+            EnderecoModel endereco = _enderecoRepository.ListarPorId(id_endereco);
+            return View(endereco);
+        }
+
+        public IActionResult Apagar(int id_endereco)
+        {
+            _enderecoRepository.Apagar(id_endereco);
+            return RedirectToAction("Index", "Pessoa");
+        }
+
+        [HttpPost]
+        public IActionResult Criar(EnderecoModel endereco)
+        {
+            _enderecoRepository.Adicionar(endereco);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(EnderecoModel endereco)
+        {
+            _enderecoRepository.Atualizar(endereco);
+            return RedirectToAction("Index");
         }
     }
 }
