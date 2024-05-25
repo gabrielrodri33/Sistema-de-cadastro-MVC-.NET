73141012aa75884b206ee200943ca4c349c77b56
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-25 12:59:22 -0300

    Apenas criado as controllers de Endereco e Pessoa (próxima feature)

---
+++ Cp3-Cadastro/Controllers/PessoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cp3_Cadastro.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
