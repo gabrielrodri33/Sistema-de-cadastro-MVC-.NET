e55a4fe9a9fb28cb14564808d801be3c624c7bf9
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-25 10:48:11 -0300

    Add project files.

---
+++ Cp3-Cadastro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cp3_Cadastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
