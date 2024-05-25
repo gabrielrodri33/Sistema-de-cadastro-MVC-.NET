0a317361e8681bf6dbe3834eb90b9305eaab74cb
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-25 12:06:12 -0300

    Criação do OracleDbContext

---
+++ Cp3-Cadastro/Persistence/OracleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Cp3_Cadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cp3_Cadastro.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<PessoaModel> Pessoa { get; set; }
+
+        public DbSet<EnderecoModel> Endereco { get; set; }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
