4d4b01cd9f4a180f62c198cab1a80e66c95947fd
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-26 17:07:35 -0300

    Criado HomeViewModel para calcular o total de pessoas e enderecos

---
+++ Cp3-Cadastro/ViewModel/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Cp3_Cadastro.ViewModel
+{
+    public class HomeViewModel
+    {
+        public int TotalPessoas {  get; set; }
+        public int TotalEnderecos { get; set; }
+    }
+}
